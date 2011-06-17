68e6a5cbd099214ecaedda5507b4abbc73d07239
Josef Šimánek <retro@ballgag.cz>
2011-06-17 13:13:06 +0200

    :( nil and + are not friends

--- app/helpers/ckeditor_helper.rb
+++ app/helpers/ckeditor_helper.rb
@@ -2,7 +2,7 @@ module CkeditorHelper
   include ActionView::Helpers::FormTagHelper
   
   def editor_tag(name, content = nil, options = {})
-    options[:class] = options[:class] + ' ckeditor'
+    options[:class] = options[:class].to_s + ' ckeditor'
     text_area_tag name, content, options
   end
   
