971687cedff9917d94959773b433c2d386eaa1ce
Josef Šimánek <retro@ballgag.cz>
2011-08-30 18:54:32 +0200

    EmbedMedia plugin added

--- public/javascripts/ckeditor/boot_ck.js
+++ public/javascripts/ckeditor/boot_ck.js
@@ -8,7 +8,7 @@
 	//	],i
     filebrowserImageBrowseUrl: '/refinery/images/insert?modal=true',
     filebrowserLinkBrowseUrl: '/refinery/pages_dialogs/link_to?wymeditor=true',
-    extraPlugins: 'stylesheetparser',
+    extraPlugins: 'stylesheetparser,MediaEmbed',
     contentsCss: '/stylesheets/ckeditor/suneditor.css',
     entities: false
 	};
